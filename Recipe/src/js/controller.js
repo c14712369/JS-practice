0e03bc5c76ee56816d62dbb690385409c2bd0c48
c14712369 <c14712369@gmail.com>
2023-01-19 16:20:20 +0800

    0119

--- Recipe/src/js/controller.js
+++ Recipe/src/js/controller.js
@@ -2,7 +2,9 @@
 // pracel 1
 import icons from 'url:../img/icons.svg';
 // pracel2
-// after parcel，原本圖片路徑會變，
+// after parcel，原本圖片路徑會變
+import 'core-js/stable';
+import 'regenerator-runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
@@ -18,8 +20,20 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
+const renderSpinner = function (parentEl) {
+  const html = `
+  <div class="spinner">
+    <svg>
+      <use href="${icons}#icon-loader"></use>
+    </svg>
+  </div>`;
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', html);
+};
+
 const showRecipe = async function () {
   try {
+    renderSpinner(recipeContainer);
     // loading recipr
     const response = await fetch(
       'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
